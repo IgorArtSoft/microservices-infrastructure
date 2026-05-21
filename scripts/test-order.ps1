f06c84a7b0e414b1f60f140711646bdfd5868496
igorart7 <igorart7@gmail.com>
2026-05-21 00:58:50 -0400

    Added usefull commands doc

--- scripts/test-order.ps1
+++ scripts/test-order.ps1
@@ -2,7 +2,7 @@ $response = Invoke-WebRequest `
   -Uri "http://localhost:8081/orders" `
   -Method Post `
   -ContentType "application/json" `
-  -Body '{"orderId":"ORD-1002","customerId":"CUST-888","amount":256.50}'
+  -Body '{"orderId":"ORD-1009","customerId":"CUST-1009","amount":1234.56}'
 
 $response.StatusCode
 $response.Content
